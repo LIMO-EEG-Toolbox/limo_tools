33f22361451066b18f168129ebb5c7384e075978
Cyril Pernet <wamcyril@gmail.com>
2017-07-01 02:24:14 +0100

    updating the cholesky decomposition

--- limo_decomp.m
+++ limo_decomp.m
@@ -1,38 +1,57 @@
-function [eigen_vectors,eigen_values] = limo_decomp(E,H)
+function [eigen_vectors,eigen_values] = limo_decomp(E,H,type)
 
-% this function is used to decompose inv(E)*H
+% FORMAT: [eigen_vectors,eigen_values] = limo_decomp(E,H,type)
 %
-% Following Rencher 2002 (Methods of multivariate
-% analysis - Wiley) we note that eig(inv(E)*H) =
-% eig((E^1/2)*H*inv(E^1/2)) = eig(inv(U')*H*inv(U))
+% INPUT E and H are matrices, typically square symmetric Sum of Squares and
+%       Cross Products
+%       type is 'Chol' (default) or 'SVD')
 %
-% E^1/2 is the square root matrix of E and U'U = E
-% (Cholesky factorization). Using the Cholesky 
-% factorisation, we return positve eigen values
-% from inv(U')*H*inv(U) which is positive semidefinite.
-% If this procedre fails (E is not positive definite) 
-% we then use an SVD decomposition
+% OUTPUT: the eigen vectors and values of the decomposition of inv(E)*H
 %
-% Cyril Pernet v2 29-05-2009
+% Following Rencher 2002 (Methods of multivariate analysis - Wiley) we note
+% that eig(inv(E)*H) = % eig((E^1/2)*H*inv(E^1/2)) = eig(inv(U')*H*inv(U))
+% and E^1/2 is the square root matrix of E and U'U = E (Cholesky factorization).
+% Using the Cholesky factorisation, we return positve eigen values from
+% inv(U')*H*inv(U) which is positive semidefinite. If this procedre fails
+% (E is not positive definite) we then use an eigen value decomposition of pinv(E)*H
+% It is also possible to procede using an SVD decomposition using the argument
+% type ('SVD')
+%
+% Cyril Pernet 2009
+% Cyril Pernet and Iege Bassez 2017
 % -----------------------------
 %  Copyright (C) LIMO Team 2010
 
-try
-    U = chol(E);
-    [eigen_vectors D] = eig(inv(U')*H*inv(U));
-    eigen_values = diag(D);
+% check input
+if nargin < 2
+    error('not enough arguments in')
+elseif nargin == 2
+    type = 'Chol';
+end
 
-catch
+% proceede
+if strcmpi(type,'chol')
+    try
+        U = chol(E);
+        [eigen_vectors, D] = eig(inv(U')*H*inv(U));
+        eigen_values = diag(D);
+        
+    catch
+        [eigen_vectors, eigen_values] = eig((pinv(E)*H));
+    end
+    
+    
+elseif strcmpi(type,'SVD')
     y = (pinv(E)*H);
-    [m n]   = size(y);
+    [m, n]   = size(y);
     if m > n
-        [v s v] = svd(y*y');
+        [v,s,v] = svd(y*y');
         s       = diag(s);
         v       = v(:,1);
         u       = y*v/sqrt(s(1));
         eigen_vectors = v;
     else
-        [u s u] = svd(y'*y);
+        [u, s,u] = svd(y'*y);
         s       = diag(s);
         u       = u(:,1);
         v       = y'*u/sqrt(s(1));
@@ -41,3 +60,4 @@ catch
     d  = sign(sum(v)); u = u*d;
     eigen_values  = u*sqrt(s(1)/n);
 end
+
