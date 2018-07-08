e97071f5211a6948424d74bbb5e6a1d826818412
arno <>
2018-07-08 07:05:54 +0200

    eegplugin_limo.m, change EEGLAB menu

--- eegplugin_limo.m
+++ eegplugin_limo.m
@@ -13,17 +13,17 @@ function version = eegplugin_limo(fig,try_strings,catch_strings)
 version ='LIMO EEG 2.0';
 if nargin < 3
     error('eegplugin_limo requires 3 arguments');
-end;
-
+end
 
 % add folder to path
 % ------------------
-    if ~exist('pop_loadeep')
-        p = which('eegplugin_limo.m');
-        p = p(1:findstr(p,'eegplugin_limo.m')-1);
-        addpath( p );
-    end;
-    
+if ~exist('pop_loadeep')
+    p = which('eegplugin_limo.m');
+    p = p(1:findstr(p,'eegplugin_limo.m')-1);
+    addpath( p );
+    addpath( fullfile(p, 'external') );
+end
+
 % create menus
 % ------------
 menu=findobj(fig,'tag','tools');
@@ -36,7 +36,7 @@ uimenu(submenu,'Label', 'LIMO EEG tools','callback','limo_tools','userdata','stu
 
 % create STUDY menus for EEGLAB 15
 % --------------------------------
-[vers,vnum] = eeg_getversion;
+[~,vnum] = eeg_getversion;
 if isempty(vnum) || vnum >= 15
     menu=findobj(fig,'tag','study');
     onstudy      = 'startup:off;epoch:off;continuous:off;study:on';
@@ -46,12 +46,13 @@ if isempty(vnum) || vnum >= 15
     e_plot_study = [e_catch 'if ~isempty(LASTCOM), STUDY = STUDYTMP; STUDY = eegh(LASTCOM, STUDY); disp(''Done.''); end; clear ALLEEGTMP STUDYTMP; eeglab(''redraw'');']; % ALLEEG not modified
     
     cb_limorunchan = [ nocheck '[STUDYTMP LASTCOM]= pop_limo(STUDY, ALLEEG,''dat'');' e_plot_study];
-    cb_limoreschan = [ nocheck 'pop_limoresults(STUDY,''dat'');' e_hist];
     cb_limoruncomp = [ nocheck '[STUDYTMP LASTCOM]= pop_limo(STUDY, ALLEEG,''ica'');' e_plot_study];
-    cb_limorescomp = [ nocheck 'pop_limoresults(STUDY,''ica'');' e_hist];
-    limo_eeg = uimenu(menu,'Label','LInear MOdeling of EEG Data (BETA TESTING ONLY)' , 'userdata', onstudy,'Tag','limoeeg','separator', 'on');
-    uimenu( limo_eeg,  'Label', 'Estimate Model Parameters (channnel)'        , 'userdata', onstudy, 'CallBack', cb_limorunchan);
-    uimenu( limo_eeg,  'Label', 'Linear Model Results  (channnel)'            , 'userdata', onstudy, 'CallBack', cb_limoreschan);
-    uimenu( limo_eeg,  'Label', 'Estimate Model Parameters (components)'     , 'userdata', onstudy, 'CallBack', cb_limoruncomp,'separator', 'on');
-    uimenu( limo_eeg,  'Label', 'Linear Model Results (components)'          , 'userdata', onstudy, 'CallBack', cb_limorescomp);
+%     cb_limoreschan = [ nocheck 'pop_limoresults(STUDY,''dat'');' e_hist];
+%     cb_limorescomp = [ nocheck 'pop_limoresults(STUDY,''ica'');' e_hist];
+    limo_eeg = uimenu(menu,'Label','LInear MOdeling of EEG Data (BETA)' , 'userdata', onstudy,'Tag','limoeeg','separator', 'on');
+    uimenu( limo_eeg, 'Label', 'Estimate Model Parameters (channnel)'        , 'userdata', onstudy, 'CallBack', cb_limorunchan);
+    uimenu( limo_eeg, 'Label', 'Estimate Model Parameters (components)'     , 'userdata', onstudy, 'CallBack', cb_limoruncomp);
+    uimenu( limo_eeg, 'Label', '2nd level analysis','callback','limo_random_effect', 'userdata', onstudy,'separator', 'on');
+    uimenu( limo_eeg, 'Label', 'LIMO EEG results (1st and 2nd level)','callback','limo_results', 'userdata', onstudy);
+    
 end
