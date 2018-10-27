d80a986aef693c1e9de9741b413a467e266b8d69
Cyril Pernet <wamcyril@gmail.com>
2018-10-27 11:48:31 +0100

    tidy up

--- limo_IRLS.m
+++ limo_IRLS.m
@@ -1,4 +1,4 @@
-function [b w] = limo_IRLS(X,Y,tune)
+function [b,w] = limo_IRLS(X,Y,tune)
 
 % LIMO_IRLS Limo Iteratively Reweighted Least Squares (IRLS)
 % IRLS is used to find the maximum likelihood estimates of a generalized
@@ -33,6 +33,7 @@ function [b w] = limo_IRLS(X,Y,tune)
 % -----------------------------
 % Copyright (C) LIMO Team 2015
 
+%% input parameters
 if  nargin < 2      
     error(message('Too Few Inputs'));      
 end 
@@ -46,22 +47,25 @@ if (rows <= cols)
    error('IRLS cannot be computed, there is not enough trials for this design');     
 end
 
+%% solve
 % Find Ordinary Least Squares
 b = pinv(X)*Y;
 
 % H - Hat matrix; Leverages for each observation
 H = diag(X*pinv(X'*X)*X');
+
 % Adjustment factor
-adjfactor = 1 ./ sqrt(1-H);
+adjfactor                 = 1 ./ sqrt(1-H);
 adjfactor(adjfactor==Inf) = 1; % when H=1 do nothing
 
+% iterate
 numiter = 0; iterlim = 100; % set a 100 iteration max
 oldRes=1; newRes=10;
 
 while(max(abs(oldRes-newRes)) > (1E-4))
    
    numiter = numiter+1;
-   oldRes = newRes;
+   oldRes  = newRes;
    
    if (numiter>iterlim)
       warning('Limo_IRLS could not converge');
@@ -69,36 +73,28 @@ while(max(abs(oldRes-newRes)) > (1E-4))
    end
    
    % Get residuals from previous fit
-   res = Y - X*b;
+   res    = Y - X*b;
    resadj = res .* repmat(adjfactor, 1, size(Y,2));
 
-   %re - Robust Estimator
+   % Robust Estimator
    % 0.6745 is the 0.75- quantile of the standard normal distribution
    % (makes the estimate unbiased)
-   re = median(abs(resadj)) ./ 0.6745;
+   re                  = median(abs(resadj)) ./ 0.6745;
    re(find(re < 1e-5)) = 1e-5;
-   r= resadj ./ repmat(tune.*re, size(Y,1),1);
+   r                   = resadj ./ repmat(tune.*re, size(Y,1),1);
    
    % Compute new weights 
-   w = (abs(r)<1) .* (1 - r.^2).^2;
+   w  = (abs(r)<1) .* (1 - r.^2).^2;
    yw = Y .* w;
-   w = sqrt(w);
+   w  = sqrt(w);
    
    for i= 1: size(Y,2)
        xw = X .* repmat(w(:,i), 1, size(X,2));
        b(:,i) = pinv(xw)*yw(:,i);
    end
    
-   % newRes= sum(sum(res.^2));
+   % update residuals
    newRes= sum(res(:).^2);
-   
-%    % plot
-%    if numiter == 1
-%        figure; xx = zeros(100,1);
-%    end
-%    xx(numiter) = newRes;
-%    plot([1:100],xx); drawnow
-   
 end
 
-end
+
