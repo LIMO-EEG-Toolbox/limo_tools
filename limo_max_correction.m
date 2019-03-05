1c5138f5aab8f63b03fe43fdab30d4c417dfd4c2
Cyril Pernet <wamcyril@gmail.com>
2019-03-05 20:34:25 +0000

    fixed max correction p value

--- limo_max_correction.m
+++ limo_max_correction.m
@@ -80,7 +80,8 @@ elseif ndims(bootM) == 2
     % get the equivalent bootstrapped p-value
     smalest_pval = 1/nboot;
     for row =1:a
-        p_val(row) = (sum(M(row) <= sortmaxM) / nboot);
+        tmp = sum(M(row,column) <= sortmaxM) / nboot;
+        p_val(row,column) = min([tmp 1-tmp]);
         if p_val(row) == 0; p_val(row) = smalest_pval; end
 
     end
