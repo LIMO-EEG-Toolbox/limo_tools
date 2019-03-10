23715ae9b0d6535f7e531702d7ff1f6caea06bb2
Cyril Pernet <wamcyril@gmail.com>
2019-03-10 22:26:04 +0000

    tfce bug on 1D vs 2D condition effect

--- limo_eeg.m
+++ limo_eeg.m
@@ -902,7 +902,8 @@ switch varargin{1}
                             cd('TFCE'); fprintf('Creating Condition %g TFCE scores \n',i)
                             if size(Condition_effect,1) == 1
                                 tfce_score(1,:) = limo_tfce(1, squeeze(Condition_effect(:,:,1)),LIMO.data.neighbouring_matrix);
-                                tfce_score = limo_tfce(2, squeeze(Condition_effect(:,:,1)),LIMO.data.neighbouring_matrix);
+                            else
+                                tfce_score      = limo_tfce(2, squeeze(Condition_effect(:,:,1)),LIMO.data.neighbouring_matrix);
                             end
                             full_name = sprintf('tfce_%s',name); save(full_name,'tfce_score');
                             clear Condition_effect tfce_score; cd ..
