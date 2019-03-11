65b357f9dab62b950c8b5d70f6faa5c052604100
cyril.pernet@ed.ac.uk <wamcyril@gmail.com>
2019-03-11 20:40:05 +0000

    added chanlocs and expected_chanlocs to load lables

--- limo_color_images.m
+++ limo_color_images.m
@@ -90,7 +90,11 @@ else
         for i = 1:length(LIMO.data.chanlocs)
             if LIMO.Level == 2
                 try
-                    label_electrodes{i} = LIMO.data.expected_chanlocs(i).labels;
+                    if isfield(LIMO.data,'chanlocs')
+                        label_electrodes{i} = LIMO.data.chanlocs(i).labels;
+                    else
+                        label_electrodes{i} = LIMO.data.expected_chanlocs(i).labels;
+                    end
                 catch
                     label_electrodes{i} = i;
                 end
