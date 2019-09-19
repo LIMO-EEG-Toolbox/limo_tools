a8419ade563c1092a43d5985feaab14b100f8986
cyril.pernet@ed.ac.uk <wamcyril@gmail.com>
2019-09-19 11:06:19 +0100

    faster reducing dim + distance entry optional

--- limo_neighbourdist.m
+++ limo_neighbourdist.m
@@ -1,27 +1,34 @@
-function channeighbstructmat = limo_neighbourdist(EEG)
+function channeighbstructmat = limo_neighbourdist(EEG,neighbourdist)
 
 % This function creates a neighbourhood 
 % distance matrix used to control for multiple comparisons.
-%
-% For explanations:
-% >>help limo_ft_neighbourselection
-%
-% Maris, E., & Oostenveld, R. (2007). 
-% Nonparametric statistical testing of EEG- and MEG-data. 
-% J Neurosci Methods, 164(1), 177-190.
-%
+% For explanations see limo_ft_neighbourselection
 % http://fieldtrip.fcdonders.nl/tutorial/cluster_permutation_timelock
 %
-% See also LIMO_FT_NEIGHBOURSELECTION
+% FORMAT: channeighbstructmat = limo_neighbourdist(EEG,neighbourdist)
+%
+% INPUT EEG the eeglab structure
+%       neighbourdist (optional) the distance between electrodes
+%
+% OUTOUT: channeighbstructmat a structure with the channel info and the
+%         neigbouring matrix
 %
 % Guillaume Rousselet v1 11 July 2010
 % adapted from code by Arnaud Delorme
 % ------------------------------
 %  Copyright (C) LIMO Team 2019
 
-tmpcfg = limo_eeglab2fieldtrip(EEG, 'preprocessing', 'none');
-lay = limo_ft_prepare_layout(tmpcfg, tmpcfg); % fieldtrip function
-tmpcfg.layout        = lay;
-neighbourdist = eval(cell2mat(inputdlg('enter neighbourhood distance','neighbourhood distance'))); % 0.37 for biosemi 128;
-tmpcfg.neighbourdist = neighbourdist;
-[neighbours,channeighbstructmat] = limo_ft_neighbourselection(tmpcfg, []); % fieldtrip function
+% reduce data array (thx Benedikt Ehinger)
+EEG.data   = EEG.data(:,1,1);
+EEG.trials = 1;
+EEG.pnts   = 1;
+
+% use fieldtrip for the layout and make the matrix
+tmpcfg        = limo_eeglab2fieldtrip(EEG, 'preprocessing', 'none');
+lay           = limo_ft_prepare_layout(tmpcfg, tmpcfg); % fieldtrip function
+tmpcfg.layout = lay;
+if nargin == 1
+    neighbourdist = eval(cell2mat(inputdlg('enter neighbourhood distance','neighbourhood distance'))); % 0.37 for biosemi 128;
+end
+tmpcfg.neighbourdist             = neighbourdist;
+[neighbours,channeighbstructmat] = limo_ft_neighbourselection(tmpcfg, []); % fieldtrip functio
