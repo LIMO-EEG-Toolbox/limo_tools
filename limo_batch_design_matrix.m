58883aca17457a68f23484a616975f8e3e8d3142
cyril.pernet@ed.ac.uk <wamcyril@gmail.com>
2019-11-17 14:30:42 +0000

    optimized loops

--- limo_batch_design_matrix.m
+++ limo_batch_design_matrix.m
@@ -11,7 +11,8 @@ function limo_batch_design_matrix(LIMOfile)
 %  Copyright (C) LIMO Team 2019
 
 global EEGLIMO
-load(LIMOfile);
+LIMO = load(LIMOfile);
+LIMO = LIMO.LIMO;
 if exist('EEGLIMO','var') && ~isempty(EEGLIMO)
     if ~strcmp([LIMO.data.data_dir filesep LIMO.data.data],[EEGLIMO.filepath filesep EEGLIMO.filename])
         cd (LIMO.data.data_dir);
@@ -45,7 +46,7 @@ if strcmp(LIMO.Analysis,'Time')
                 signal = limo_concatcells(signal);
             end
         else
-            signal = eeg_getdatact(EEGLIMO,'component',[1:size(EEGLIMO.icawinv,2)]);
+            signal = eeg_getdatact(EEGLIMO,'component',1:size(EEGLIMO.icawinv,2));
         end
         Y = signal(:,LIMO.data.trim1:LIMO.data.trim2,:); clear signal
         
@@ -79,30 +80,31 @@ if strcmp(LIMO.Analysis,'Time')
             Y = newY; clear newY;
         end
     else % channels
-        try isfield(EEGLIMO.etc, 'datafiles') && isfield(EEGLIMO.etc.datafiles,'daterp');
+        if isfield(EEGLIMO.etc, 'datafiles') && isfield(EEGLIMO.etc.datafiles,'daterp')
             if ~iscell(EEGLIMO.etc.datafiles.daterp) && strcmp(EEGLIMO.etc.datafiles.daterp(end-3:end),'.mat')
                 Y = load(EEGLIMO.etc.datafiles.daterp);
                 if isstruct(Y)
                     Y = getfield(Y,cell2mat(fieldnames(Y)));
                 end
             else
-                for d=1:length(EEGLIMO.etc.datafiles.daterp)
+                for d=length(EEGLIMO.etc.datafiles.daterp):-1:1
                     Y{d} = load('-mat',cell2mat(EEGLIMO.etc.datafiles.daterp(d)));
                     if isstruct(Y{d}); Y{d}  = limo_struct2mat(Y{d}); end
                 end
                 Y = limo_concatcells(Y);
             end
-        catch
+        else
             disp('the field EEG.etc.datafiles.daterp pointing to the data is missing - using hack')
-            try
-                cd(LIMO.data.data_dir); erp = dir('*.daterp');
-                for d=1:length(erp)
+            cd(LIMO.data.data_dir); 
+            erp = dir('*.daterp');
+            if ~isempty(erp)
+                for d=length(erp):-1:1
                     Y{d} = load('-mat',erp(d).name);
-                    if isstruct(Y{d}); Y{d}  = limo_struct2mat(Y{d}); end
+                    if isstruct(Y{d})
+                        Y{d}  = limo_struct2mat(Y{d}); 
+                    end
                 end
                 Y = limo_concatcells(Y);
-            catch
-                Y = EEGLIMO.data;
             end
         end
         Y = Y(:,LIMO.data.trim1:LIMO.data.trim2,:);
@@ -131,7 +133,7 @@ elseif strcmp(LIMO.Analysis,'Frequency')
                 signal = limo_concatcells(signal);
             end
         else
-            signal = eeg_getdatact(EEGLIMO,'component',[1:length(EEGLIMO.icawinv)]);
+            signal = eeg_getdatact(EEGLIMO,'component',1:length(EEGLIMO.icawinv));
         end
         Y = signal(:,LIMO.data.trim1:LIMO.data.trim2,:); clear signal
         if isfield(LIMO.data,'cluster')
@@ -140,14 +142,14 @@ elseif strcmp(LIMO.Analysis,'Frequency')
             catch
                 error('to run component clustering, you need the EEGLAB study loaded in the workspace with the clustering computed and saved')
             end
-            nb_clusters = size(STUDY.cluster(1).child,2);
-            nb_subjects = length({STUDY.datasetinfo.subject}); % length(unique({STUDY.datasetinfo.subject}));
-            Cluster_matrix = parse_clustinfo(STUDY,STUDY.cluster(1).name);
-            dsetinfo = rel2fullpath(STUDY.filepath,{STUDY.datasetinfo.filepath}');
-            data_dir = rel2fullpath(STUDY.filepath,LIMO.data.data_dir(1:end));
+            nb_clusters     = size(STUDY.cluster(1).child,2);
+            nb_subjects     = length({STUDY.datasetinfo.subject}); % length(unique({STUDY.datasetinfo.subject}));
+            Cluster_matrix  = parse_clustinfo(STUDY,STUDY.cluster(1).name);
+            dsetinfo        = rel2fullpath(STUDY.filepath,{STUDY.datasetinfo.filepath}');
+            data_dir        = rel2fullpath(STUDY.filepath,LIMO.data.data_dir(1:end));
             current_subject = find(cellfun(@strcmp, dsetinfo',repmat({data_dir},nb_subjects,1)));
-            subject_name = {STUDY.datasetinfo(current_subject(1)).subject};
-            newY = NaN(nb_clusters,size(Y,2),size(Y,3));
+            subject_name    = {STUDY.datasetinfo(current_subject(1)).subject};
+            newY            = NaN(nb_clusters,size(Y,2),size(Y,3));
             for c=1:nb_clusters
                 n = length(Cluster_matrix.clust(c).subj);
                 tmp = find(cellfun(@strcmp,Cluster_matrix.clust(c).subj',repmat(subject_name,n,1)));
@@ -170,7 +172,7 @@ elseif strcmp(LIMO.Analysis,'Frequency')
                     Y = getfield(Y,cell2mat(fieldnames(Y)));
                 end
             else
-                for d=1:length(EEGLIMO.etc.datafiles.datspec)
+                for d=length(EEGLIMO.etc.datafiles.datspec):-1:1
                     Y{d} = load('-mat',cell2mat(EEGLIMO.etc.datafiles.datspec(d)));
                     if isstruct(Y{d}); Y{d}  = limo_struct2mat(Y{d}); end
                 end
@@ -178,53 +180,45 @@ elseif strcmp(LIMO.Analysis,'Frequency')
             end
         else
             disp('the field EEG.etc.datspec pointing to the data is missing - using a hack')
-            try
-                cd(LIMO.data.data_dir); spec = dir('*.datspec');
-                for d=1:length(spec)
+            cd(LIMO.data.data_dir); 
+            spec = dir('*.datspec');
+            if ~isempty(spec)
+                for d=length(spec):-1:1
                     Y{d} = load('-mat',spec(d).name);
                     if isstruct(Y{d}); Y{d}  = limo_struct2mat(Y{d}); end
                 end
                 Y = limo_concatcells(Y);
-            catch
-                Y = EEGLIMO.data;
             end
         end
         Y = Y(:,LIMO.data.trim1:LIMO.data.trim2,:);
     end
     
 elseif strcmp(LIMO.Analysis,'Time-Frequency')
-    disp('Time-Frequency implementation - loading tf data, be patient ...');
+    disp('Time-Frequency implementation - checking tf data, be patient ...');
     
     if strcmp(LIMO.Type,'Components')
         if ~iscell(EEGLIMO.etc.datafiles.datspec) && isfield(EEGLIMO.etc.datafiles,'icatimef')
-            try
-                signal = load('-mat',EEGLIMO.etc.datafiles.icatimef);
-                if isstruct(signal); signal  = limo_struct2mat(signal); end
-            catch
-                for d=1:length(EEGLIMO.etc.datafiles.icaspec)
-                    signal{d} = load('-mat',cell2mat(EEGLIMO.etc.datafiles.icatimef(d)));
-                    if isstruct(signal{d}); signal{d}  = limo_struct2mat(signal{d}); end
-                end
-            end
-            signal = limo_concatcells(signal);
+             signal = abs(limo_struct2mat(EEGLIMO.etc.datafiles.icatimef)).^2;
         else
-            signal = eeg_getdatact(EEGLIMO,'component',[1:length(EEGLIMO.icawinv)]);
+            signal = abs(eeg_getdatact(EEGLIMO,'component',1:length(EEGLIMO.icawinv)).^2);
         end
+        
         Y = signal(:,LIMO.data.trim_low_f:LIMO.data.trim_high_f,LIMO.data.trim1:LIMO.data.trim2,:); clear signal
+        
         if isfield(LIMO.data,'cluster')
             try
                 STUDY = evalin('base','STUDY');
             catch
                 error('to run component clustering, you need the EEGLAB study loaded in the workspace with the clustering computed and saved')
             end
-            nb_clusters = size(STUDY.cluster(1).child,2);
-            nb_subjects = length(unique({STUDY.datasetinfo.subject}));
-            Cluster_matrix = parse_clustinfo(STUDY,STUDY.cluster(1).name);
-            dsetinfo = rel2fullpath(STUDY.filepath,{STUDY.datasetinfo.filepath}');
-            data_dir = rel2fullpath(STUDY.filepath,LIMO.data.data_dir(1:end));
+            nb_clusters     = size(STUDY.cluster(1).child,2);
+            nb_subjects     = length(unique({STUDY.datasetinfo.subject}));
+            Cluster_matrix  = parse_clustinfo(STUDY,STUDY.cluster(1).name);
+            dsetinfo        = rel2fullpath(STUDY.filepath,{STUDY.datasetinfo.filepath}');
+            data_dir        = rel2fullpath(STUDY.filepath,LIMO.data.data_dir(1:end));
             current_subject = find(cellfun(@strcmp, dsetinfo',repmat({data_dir},nb_subjects,1)));
-            subject_name = {STUDY.datasetinfo(current_subject).subject};
-            newY = NaN(nb_clusters,size(Y,2),size(Y,3));
+            subject_name    = {STUDY.datasetinfo(current_subject).subject};
+            newY            = NaN(nb_clusters,size(Y,2),size(Y,3));
             for c=1:nb_clusters
                 n = length(Cluster_matrix.clust(c).subj);
                 tmp = find(cellfun(@strcmp,Cluster_matrix.clust(c).subj',repmat(subject_name,n,1)));
@@ -241,32 +235,24 @@ elseif strcmp(LIMO.Analysis,'Time-Frequency')
         end
     else % channels
         if isfield(EEGLIMO.etc, 'datafiles') && isfield(EEGLIMO.etc.datafiles,'dattimef')
-            for d=1:length(EEGLIMO.etc.datafiles.dattimef)
-                Y{d} = load('-mat',cell2mat(EEGLIMO.etc.datafiles.dattimef(d)));
-                if isstruct(Y{d}); Y{d}  = limo_struct2mat(Y{d}); end
-            end
-            Y = limo_concatcells(Y);
-        elseif isfield(EEGLIMO.etc.datafiles,'datersp')
+            Y = abs(limo_struct2mat(EEGLIMO.etc.datafiles.dattimef)).^2;
+        elseif isfield(EEGLIMO.etc, 'datafiles') && isfield(EEGLIMO.etc.datafiles,'datersp')
             [~,~,ext]=fileparts(EEGLIMO.etc.datafiles.datersp);
-            if strcmp(ext,'.dattimef')
-                [Y,TimeVect,FreqVect] = limo_struct2mat(EEGLIMO.etc.datafiles.datersp);
+            if strcmp(ext,'.dattimef') % somehow wrong name
+                Y = abs(limo_struct2mat(EEGLIMO.etc.datafiles.dattimef)).^2;
             else
-                Y = load(EEGLIMO.etc.datafiles.datersp);
-                if isstruct(Y)
-                    Y = getfield(Y,cell2mat(fieldnames(Y)));
-                end
+                Y = abs(load(EEGLIMO.etc.datafiles.datersp)).^2;
             end
         else
-            disp('no data found, the field EEG.etc.dattimef or EEGLIMO.etc.datersp pointing to the data is missing - using a hack')
-            try
-                cd(LIMO.data.data_dir); ersp = dir('*.dattimef');
-                for d=1:length(ersp)
+            disp('no data found: using a hack searching for dattimef data')
+            cd(LIMO.data.data_dir); 
+            ersp = dir('*.dattimef');
+            if ~isempty(ersp)
+                for d=length(ersp):-1:1
                     Y{d} = load('-mat',ersp(d).name);
                     if isstruct(Y{d}); Y{d}  = limo_struct2mat(Y{d}); end
                 end
-                Y = limo_concatcells(Y);
-            catch
-                Y = EEGLIMO.data;
+                Y = abs(limo_concatcells(Y)).^2;
             end
         end
         Y = Y(:,LIMO.data.trim_low_f:LIMO.data.trim_high_f,LIMO.data.trim1:LIMO.data.trim2,:);
@@ -307,19 +293,30 @@ function file_fullpath = rel2fullpath(studypath,filepath)
 % fit the one of 'filepath'. That means that if 'filepath' is a cell array,
 % then the output will a cell array too, and the same if is a string.
 
-nit = 1; if iscell(filepath), nit = length(filepath);end
+nit = 1; 
+if iscell(filepath)
+    nit = length(filepath);
+end
 
-for i = 1:nit
-    if iscell(filepath),pathtmp = filepath{i}; else pathtmp = filepath; end
-    if strfind(pathtmp(end),filesep), pathtmp = pathtmp(1:end-1); end % Getting rid of filesep at the end
+for i = nit:-1:1
+    if iscell(filepath)
+        pathtmp = filepath{i}; 
+    else
+        pathtmp = filepath; 
+    end
+    
+    if strfind(pathtmp(end),filesep)
+        pathtmp = pathtmp(1:end-1); 
+    end % Getting rid of filesep at the end
+    
     if strfind(pathtmp(1:2),['.' filesep])
-        if iscell(filepath),
+        if iscell(filepath)
             file_fullpath{i} = fullfile(studypath,pathtmp(3:end));
         else
             file_fullpath = fullfile(studypath,pathtmp(3:end));
         end
     else
-        if iscell(filepath),
+        if iscell(filepath)
             file_fullpath{i} = pathtmp;
         else
             file_fullpath = pathtmp;
