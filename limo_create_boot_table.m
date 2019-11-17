75c6c410f935d0854061d5e562ac30c8a432b5bd
cyril.pernet@ed.ac.uk <wamcyril@gmail.com>
2019-11-17 14:33:20 +0000

    tidying up

--- limo_create_boot_table.m
+++ limo_create_boot_table.m
@@ -1,42 +1,41 @@
 function boot_table = limo_create_boot_table(data,nboot)
 
-% this function allows building a table of data to resample
-% such as almost the same resampling is applied across electrodes.
-% At the 2nd level, not all subjects have the same electrodes, and
-% missing data are treated as NaN. The boot_table has indexes which are
-% common to all electrodes + some other indexes specific to electrodes
-% where there some NaNs.
+% Builds a table of data to resample such as almost the same resampling is
+% applied across channels. At the 2nd level, not all subjects have the same
+% channels because missing data can betreated as NaN. The boot_table has 
+% indexes which are common to all channels + some other indexes specific
+% to channels where there some NaNs.
 %
 % FORMAT: boot_table = limo_create_boot_table(data,nboot)
 %
-% INPUT: data: a 3D matrix [electrode x time frames x subjects];
+% INPUT: data: a 3D matrix [channels x time frames x subjects];
 %        nboot: the number of bootstraps to do
-%        for repeated measures - call this function inputing 1 measure and
-%        apply to all measures the created table
+%        for repeated measures (Time-Frequency, Repeated meaures ANOVA, etc)
+%        call this function inputing 1 measure and apply to all measures the
+%        created table
 %
 % OUTPUT: boot_table is a cell array with one resampling matrix per
-% electrode
+%         channel
 %
-% Cyril Pernet v1 24-06-2013
-% v2 simple edit to specify up front Nmin
+% Cyril Pernet 
 % ------------------------------
 %  Copyright (C) LIMO Team 2019
 
 %% edit default
 Nmin = 3; % this is the minimum number of different trials/subjects 
-          % if too low, the variance is < 1 and thre stat values will be
+          % if too low, the variance is < 1 and the stat values will be
           % too high see Pernet et al. 2014
 
 %% start
 % check data for NaNs
 if size(data,1) == 1
-    chdata=data(1,1,:); 
+    chdata = data(1,1,:); 
 else
-    chdata=squeeze(data(:,1,:)); 
+    chdata = squeeze(data(:,1,:)); 
 end
 
 if sum(sum(isnan(chdata),2)==size(data,ndims(data))) ~=0
-    disp('some electrodes are empty (full of NaN) - still making the table but some cells will be empty')
+    disp('some channels are empty (full of NaN) - still making the table but some cells will be empty')
 end
 
 if (sum((size(data,3) - sum(isnan(chdata),2))<=3)) ~=0
@@ -48,7 +47,8 @@ B=1;
 boot_index=zeros(size(data,3),nboot);
 if size(data,3)-1 <= Nmin
     error(['Not enough subjects in dataset - need at least ' num2str(Nmin+2) ' subjects']);
-end;
+end
+
 while B~=nboot+1
     tmp = randi(size(data,3),size(data,3),1);
     if length(unique(tmp)) >= Nmin % at least Nmin different observations per boot 
@@ -58,23 +58,24 @@ while B~=nboot+1
 end
 clear chdata tmp
 
-% loop per electrode, if no nan use boot_index else change it
+% loop per channel, if no nan use boot_index else change it
 if size(data,1) > 1
     array = find(sum(squeeze(isnan(data(:,1,:))),2) < size(data,3)-3);
 else
-    array = [1];
-end;
-for e = 1:size(array,1)
-    electrode = array(e);
-    tmp = squeeze(data(electrode,:,:)); % 2D
-    Y = tmp(:,find(~isnan(tmp(1,:)))); % remove NaNs
-    bad_subjects = find(isnan(tmp(1,:)));
+    array = 1;
+end
+
+for e = size(array,1):-1:1
+    channel     = array(e);
+    tmp           = squeeze(data(channel,:,:)); % 2D
+    Y             = tmp(:,find(~isnan(tmp(1,:)))); % remove NaNs
+    bad_subjects  = find(isnan(tmp(1,:)));
     good_subjects = find(~isnan(tmp(1,:)));
     
     if ~isempty(bad_subjects)
         boot_index2 = zeros(size(Y,2),nboot);
         for c=1:nboot
-            common = ismember(boot_index(:,c),good_subjects');
+            common  = ismember(boot_index(:,c),good_subjects');
             current = boot_index(find(common),c); % keep resampling of good subjets
             % add or remove indices
             add = size(Y,2) - size(current,1);
@@ -89,11 +90,11 @@ for e = 1:size(array,1)
                 new_boot(find(tmp_boot > bad_subjects(i))) = tmp_boot(find(tmp_boot >  bad_subjects(i))) - i; % change range
             end
             % new boot-index
-            boot_index2(:,c) = new_boot;
+            boot_index2(:,c)  = new_boot;
         end
-        boot_table{electrode} = boot_index2;
+        boot_table{channel} = boot_index2;
     else
-        boot_table{electrode} = boot_index;
+        boot_table{channel} = boot_index;
     end
 end
 
