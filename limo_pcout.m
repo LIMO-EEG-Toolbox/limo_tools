c9bfc62c46bb36454b5269326ea8f39468dca034
CPernet <wamcyril@gmail.com>
2021-10-13 14:37:48 +0200

    p<n

--- limo_pcout.m
+++ limo_pcout.m
@@ -78,7 +78,61 @@ x2      = (x - repmat(median(x), n, 1)) ./ repmat(madx,n,1); % robust standardiz
 x3      = x2 - repmat(mean(x2),n,1); % standardization
  
 % calculate the principal components (retain 99% of variance)
-a       = svd(x3).^2./(n-1);
+if p>n
+    a       = svd(x3).^2./(n-1);
+else
+    if srcmpi(rrm,'downsample')
+        
+    elseif strcmpi(rrm,'decompose')
+        N = size(data, 2);
+        m = mean(data, 2);
+        
+        % Subtract mean from columns
+        if matlab_version_at_least(7, 4)
+            data = bsxfun(@minus, data, m);
+        else
+            for i=1:N
+                data(:,i) = data(:,i) - m;
+            end
+        end
+        
+        % Covariance matrix
+        C = (data' * data) / N;
+        
+        [pc_, eiv_] = pcacov(C);
+        
+        % Normalize eigenvalues to sum at 1
+        neiv = eiv_ ./ sum(eiv_);
+        
+        % Count number of eigenvalue needed to cover desired variation
+        covered = 0;
+        i = 1;
+        
+        while covered < var
+            covered = covered + neiv(i);
+            i = i + 1;
+        end
+        
+        % Obtain principal components of data * data' from
+        % principal components of data' * data
+        % (only select i-1 columns, as there are i-1 relevant eigenvalues)
+        pc = data * pc_(:,1:i-1);
+        
+        % Normalize columns
+        if matlab_version_at_least(7, 4)
+            pc = bsxfun(@rdivide, pc, sqrt(sum(pc.^2, 1)));
+        else
+            for j=1:size(pc,2)
+                pc(:,j) = pc(:,j) / norm(pc(:,j));
+            end
+        end
+        
+        if nargout > 1
+            eiv = eiv_(1:i-1);
+        end
+    end
+end
+
 paux    = (1:p)' .* ((cumsum(a)./sum(a) > 0.99));
 paux2   = paux(paux > 0);
 p1      = paux2(1);
