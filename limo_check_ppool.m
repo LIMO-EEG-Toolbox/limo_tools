e6a7d138bff23206afd058321fe77a0f1f087734
Arnaud Delorme <arnodelorme@gmail.com>
2022-08-03 02:55:11 -0700

    fix error when parallel toolbox absent

--- limo_check_ppool.m
+++ limo_check_ppool.m
@@ -4,8 +4,13 @@ function limo_check_ppool
 % ------------------------------
 %  Copyright (C) LIMO Team 2020
 
-p = gcp('nocreate');
-if isempty(p) % i.e. it's not on
-    parpool(getenv('NUMBER_OF_PROCESSORS')-1); % logical
-    % parpool(feature('numcores')-1); only physical
+if ~exist('gcp')
+    disp('Parallel toolbox not found - nothing to worry about (except slower computation in some cases)');
+else
+    p = gcp('nocreate');
+    if isempty(p) % i.e. it's not on
+        parpool(getenv('NUMBER_OF_PROCESSORS')-1); % logical
+        % parpool(feature('numcores')-1); only physical
+    end
 end
+
