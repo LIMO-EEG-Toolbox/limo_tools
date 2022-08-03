ad83efe802a833be582ff9953922392e729ec042
CPernet <wamcyril@gmail.com>
2022-08-03 07:04:37 +0200

    fix local cluster for hyperthreading

--- limo_check_ppool.m
+++ limo_check_ppool.m
@@ -5,7 +5,23 @@ function limo_check_ppool
 %  Copyright (C) LIMO Team 2020
 
 p = gcp('nocreate');
-if isempty(p) % i.e. it's not on
-    parpool(getenv('NUMBER_OF_PROCESSORS')-1); % logical
-    % parpool(feature('numcores')-1); only physical
+if isempty(p) % i.e. the parallel toolbox is not on
+    
+    % check how many cores to use
+    % ---------------------------
+    N = getenv('NUMBER_OF_PROCESSORS'); % logical nu,ber of cores (i.e. count hyperthreading)
+    % N = feature('numcores');          % physical number of cores
+    if ischar(N)
+        N = str2double(N);
+    end
+    
+    % check and set the local profile NumWorkers
+    % ----------------------------------
+    c            = parcluster;
+    c.NumWorkers = N;
+    saveProfile(c);
+    
+    % go
+    % --
+    parpool(N-1); % logical
 end
