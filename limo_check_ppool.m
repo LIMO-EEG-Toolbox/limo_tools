f2574481a77c4117ffd326af7cff09bbd99abeb1
CPernet <wamcyril@gmail.com>
2022-08-03 13:18:38 +0200

    more flexible
    include @arnodelorme idea to test (via ver rahter than gcp)
    update NumWorkers to avoid conflict
    allows fixed N

--- limo_check_ppool.m
+++ limo_check_ppool.m
@@ -1,32 +1,42 @@
 function limo_check_ppool
 
 % routine to make sure the parallel pool is working
-% ------------------------------
-%  Copyright (C) LIMO Team 2020
+% you can set manually N below, so that the pool is fix
+% -----------------------------------------------------
+%  Copyright (C) LIMO Team 2022
 
-if ~exist('gcp')
-    disp('Parallel toolbox not found - nothing to worry about (except slower computation in some cases)');
-else
-    p = gcp('nocreate');
-    if isempty(p) % i.e. the parallel toolbox is not on
+N      = [];
+addons = ver;
+
+if any(strcmpi('Parallel Computing Toolbox',arrayfun(@(x) x.Name, addons, "UniformOutput",false)))
     
-        % check how many cores to use
-        % ---------------------------
-        N = getenv('NUMBER_OF_PROCESSORS'); % logical nu,ber of cores (i.e. count hyperthreading)
-        % N = feature('numcores');          % physical number of cores
-        if ischar(N)
-            N = str2double(N);
+    if isempty(N)
+        p = gcp('nocreate');
+        if isempty(p) % i.e. the parallel toolbox is not already on
+            
+            % check how many cores to use
+            % ---------------------------
+            N = getenv('NUMBER_OF_PROCESSORS'); % logical number of cores (i.e. count hyperthreading)
+            % N = feature('numcores');          % physical number of cores
+            if ischar(N)
+                N = str2double(N);
+            end
+            
+            % check and set the local profile NumWorkers
+            % ----------------------------------
+            c            = parcluster;
+            c.NumWorkers = N;
+            % saveProfile(c);
+            
+            % go
+            % --
+            parpool(N-1);
+        else
+            parpool(N);
         end
-     
-        % check and set the local profile NumWorkers
-        % ----------------------------------
-        c            = parcluster;
-        c.NumWorkers = N;
-        saveProfile(c);
-     
-        % go
-        % --
-        parpool(N-1); % logical
     end
+    
+else
+    disp('Parallel toolbox not found - nothing to worry about (except slower computation in some cases)');
 end
 
