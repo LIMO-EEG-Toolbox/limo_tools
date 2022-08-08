76362e7ad23aeca5aedc7b011c60a36bc40bb32b
CPernet <wamcyril@gmail.com>
2022-08-08 15:44:40 +0200

    also take cell array in

--- limo_best_electrodes.m
+++ limo_best_electrodes.m
@@ -29,18 +29,21 @@ if nargin == 0
     [name,pathname,FilterIndex]=uigetfile({'*.mat','MAT-files (*.mat)'; '*.txt','Text (*.txt)'}, ...
         'Pick a list of result files (e.g. R2)');
 else
-   [pathname,name,ext]=fileparts(varargin{1}); name = [name ext];
-   if exist(fullfile(pathname,name),'file')
-       FilterIndex = 1;
-   else
-       warning('file %s not found',name);
-       return
-   end
+    if ischar(varargin{1})
+        [pathname,name,ext]=fileparts(varargin{1}); name = [name ext];
+        if exist(fullfile(pathname,name),'file')
+            FilterIndex = 1;
+        else
+            warning('file %s not found',name);
+            return
+        end
+    else % cell array of names
+        FilterIndex = 0;
+        name = varargin{1};
+    end
 end
 
-
 if FilterIndex ~= 0
-    
     if strcmp(name(end-3:end),'.txt')
         name = importdata(fullfile(pathname,name));
     elseif strcmp(name(end-3:end),'.mat')
@@ -55,43 +58,44 @@ if FilterIndex ~= 0
         end
     end
 else
-    disp('selection aborded')
-    return
+    if ~iscell(name)
+        disp('selection aborded')
+        return
+    end
 end
 
 
 %% now collect data
-Ns = length(name);
+Ns             = size(name,1);
 channel_vector = NaN(Ns,1);
-urchan_vector = NaN(Ns,1);
+urchan_vector  = NaN(Ns,1);
 
 for i=Ns:-1:1
     tmp = load(name{i});
     if isfield(tmp,'LIMO')
         tmp = load(fullfile(tmp.LIMO.dir,'R2.mat'));
     end
-    tmp = getfield(tmp,cell2mat(fieldnames(tmp)));
+    tmp = tmp.(cell2mat(fieldnames(tmp)));
     
-    try
+    try % read F values
         if numel(size(tmp)) == 4
             data{i} = squeeze(tmp(:,:,:,end-1)); % end-1 because R2 dim is R2,F,p
             % and condition/covariates dim are F/p
-            data_size{i} = size(data{i});
-            index = find(data{i} == max(data{i}(:)));
-            [channel_vector(i),~,~]=ind2sub(data_size{i},index);
+            data_size{i}            = size(data{i});
+            index                   = find(data{i} == max(data{i}(:)));
+            [channel_vector(i),~,~] = ind2sub(data_size{i},index(1));
         else
-            data{i} = squeeze(tmp(:,:,end-1));
-            data_size{i} = size(data{i});
-            index = find(data{i} == max(data{i}(:)));
-            [channel_vector(i),~]=ind2sub(data_size{i},index);
+            data{i}                 = squeeze(tmp(:,:,end-1));
+            data_size{i}            = size(data{i});
+            index                   = find(data{i} == max(data{i}(:)));
+            [channel_vector(i),~]   = ind2sub(data_size{i},index(1));
         end
         
-        LIMO = load([fileparts(name{i}) filesep 'LIMO.mat']); LIMO =LIMO.LIMO;
+        LIMO = load([fileparts(name{i}) filesep 'LIMO.mat']); LIMO = LIMO.LIMO;
         if ~isempty(LIMO.data.chanlocs(channel_vector(i)).urchan)
             urchan_vector(i) = LIMO.data.chanlocs(channel_vector(i)).urchan;
         end
         fprintf('subject %g analysed \n',i);
-        
     catch bug
         error('file error with subject %s\n%s',name{i},bug.message);
     end
