a491103b411443f00e04901c4d25b0bcc2f322ab
CPernet <wamcyril@gmail.com>
2022-09-10 09:37:49 +0200

    bug fix; file check for 2 samples

--- limo_random_select.m
+++ limo_random_select.m
@@ -512,7 +512,7 @@ elseif strcmpi(stattest,'two-samples t-test')
 
     % check type of files and returns which beta param to test
     % -------------------------------------------------------
-    if ~all(contains(Names{1},'con')) && ~all(contains(Names{1},'con'))
+    if ~all(contains(Names{1},'betas')) && ~all(contains(Names{1},'con'))
         % do this only if betas - for con paramters = [1 1]
         for gp = 1:2
             for sub=1:size(LIMO.data.data_dir{gp},2)
