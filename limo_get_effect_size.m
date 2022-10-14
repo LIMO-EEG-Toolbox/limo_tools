448cf392ef78cd333f3efea9876f7c120b19b750
CPernet <wamcyril@gmail.com>
2022-10-14 10:48:52 +0200

    fixed reading post-hoc contrasts

--- limo_get_effect_size.m
+++ limo_get_effect_size.m
@@ -177,6 +177,7 @@ elseif contains(LIMO.design.name,'Repeated','IgnoreCase',true)   % All stuffs fo
             F   = squeeze(F(:,:,1));
         else
             df  = squeeze(F(:,:,3));
+            dfe = size(LIMO.design.X,1)/prod(LIMO.design.repeated_measure) - df;
             F   = squeeze(F(:,:,4));
         end
     else
@@ -184,11 +185,12 @@ elseif contains(LIMO.design.name,'Repeated','IgnoreCase',true)   % All stuffs fo
             F   = squeeze(F(:,:,:,1));
         else
             df  = squeeze(F(:,:,:,3));
+            dfe = size(LIMO.design.X,1)/prod(LIMO.design.repeated_measure) - df;
             F   = squeeze(F(:,:,:,4));
         end
     end
     
-    if ~contains(filename,'Rep_ANOVA_Interaction') && ~contains(filename,'Rep_ANOVA_Gp') && ~contains(filename,'ess')
+    if ~contains(filename,'Rep_ANOVA_Interaction') && ~contains(filename,'Rep_ANOVA_Gp') 
         if contains(filename,'Main_effect','IgnoreCase',true)
             index1     = strfind(filename,'Main_effect')+length('Main_effect')+1;
             index2     = max(strfind(filename,'_'))-1;
@@ -198,19 +200,24 @@ elseif contains(LIMO.design.name,'Repeated','IgnoreCase',true)   % All stuffs fo
             index2     = max(strfind(filename,'_'))-1;
             effect_nb  = eval(filename(index1:index2));
         else
-            error('unexpected file')
+            index1     = strfind(filename,'ess')+length('ess')+1;
+            effect_nb  = eval(filename(index1:end-4));
         end
-        df          = squeeze(LIMO.design.df(:,effect_nb));
-        dfe         = squeeze(LIMO.design.dfe(:,effect_nb));
-        T2          = F.*repmat((df./dfe),1,size(F,2));
-        N           = size(LIMO.design.X,1)/size(LIMO.design.C{effect_nb},2);
-        effect_size = T2 ./ N;
+
+        if ~exist('df','var')
+            df      = repmat(squeeze(LIMO.design.df(:,effect_nb)),[1 size(F,2)]);
+            dfe     = repmat(squeeze(LIMO.design.dfe(:,effect_nb)),[1 size(F,2)]);
+        end
+        T2          = F.*(df./dfe);
+        effect_size = T2 ./ size(LIMO.design.X,1)/prod(LIMO.design.repeated_measure);
         name        = fullfile(filepath,[filename(1:end-4) '_MahalanobisD.mat']);
+   
     elseif contains(filename,'Rep_ANOVA_Gp')
         A           = (LIMO.design.group.df'.*F);
         B           = (A+repmat(LIMO.design.group.dfe',1,size(A,2)));
         effect_size = A ./B ;
         name        = fullfile(filepath,[filename(1:end-4) '_PartialEta2.mat']);
+    
     elseif contains(filename,'Rep_ANOVA_Interaction')
         effect_nb   = filename(max(strfind(filename,'_'))+1:end-4);
         position    = contains(LIMO.design.effects,'Interaction');
@@ -224,13 +231,9 @@ elseif contains(LIMO.design.name,'Repeated','IgnoreCase',true)   % All stuffs fo
         N           = size(LIMO.design.X,1)/size(LIMO.design.C{effect_nb},2);
         effect_size = T2 ./ N;
         name        = fullfile(filepath,[filename(1:end-4) '_MahalanobisD.mat']);
-    elseif contains(filename,'ess')
-        if contains(filename,'gp_interaction')
-            effect_nb   = str2double(filename(strfind(filename,'ess_gp_interaction_')+19:end-4));
-        else
-            effect_nb   = str2double(filename(strfind(filename,'ess_')+4:end-4));
-        end
-        N           = size(LIMO.design.X,1)/size(LIMO.contrast{effect_nb}.C,2);
+    
+    else
+        N           = size(LIMO.design.X,1)/prod(LIMO.design.repeated_measure);
         T2          = F.*(df./(N-df));
         effect_size = T2 ./ N;
     end
