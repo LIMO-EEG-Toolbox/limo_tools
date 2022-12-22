a3ea5cfb8f3f93a242590bb5a6f3e78e6e6044d6
CPernet <wamcyril@gmail.com>
2022-12-22 19:40:34 +0100

    move setting script to root

---
+++ limo_settings_script.m
@@ -0,0 +1,23 @@
+limo_settings.psom = true; % toggle the use of the PSOM library
+limo_settings.workdir = 'derivatives'; % empty is current folder
+limo_settings.newgui = true; % empty is current folder
+
+if exist('limo_settings_script_user')
+    eval('limo_settings_script_user')
+end
+
+if isequal(limo_settings.workdir, 'derivatives')
+    try
+        STUDY=evalin('base','STUDY');
+        %limo_settings.workdir = fullfile(S.filepath, 'derivatives');
+        limo_settings.workdir = STUDY.filepath;
+    catch
+        disp('Failed to get study');
+        limo_settings.workdir = '';
+        if ~exist('STUDY', 'var')
+            STUDY = [];
+        end
+    end
+else
+    STUDY = [];
+end
