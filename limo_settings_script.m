bfe682cbbe28d9d1924d5bcc875aa688daa1effa
CPernet <wamcyril@gmail.com>
2023-03-29 18:34:36 +0200

    add info crietria

--- limo_settings_script.m
+++ limo_settings_script.m
@@ -1,15 +1,29 @@
-limo_settings.psom = true; % toggle the use of the PSOM library
-limo_settings.workdir = 'derivatives'; % empty is current folder
-limo_settings.newgui = true; % empty is current folder
+% Script allowing to custimize LIMO tools behaviour
+%
+% Arnaud Delorme & Cyril Pernet
 
+% new GUI available for plotting and some selection menus
+limo_settings.newgui = true; 
+
+% default place where data are written is into 'derivatives'
+% set to empty to work in the current folder
+limo_settings.workdir = 'derivatives'; 
+
+% toggle the use of the PSOM library, use to batch over subjects as a
+% pipeline, ie capture errors and restarts where it failed
+limo_settings.psom = true; 
+
+% overwrite using your own script
 if exist('limo_settings_script_user')
     eval('limo_settings_script_user')
 end
 
+%% --------------------------------------------------
+%                  do not edit
+% ---------------------------------------------------
 if isequal(limo_settings.workdir, 'derivatives')
     try
         STUDY=evalin('base','STUDY');
-        %limo_settings.workdir = fullfile(S.filepath, 'derivatives');
         limo_settings.workdir = STUDY.filepath;
     catch
         disp('Failed to get study');
