1b1d3a43512f6f2ffb6199e648571e1e68bc6015
CPernet <wamcyril@gmail.com>
2024-05-06 08:40:43 +0200

    try to deal with N core 1 and not start pool if parfor

--- limo_check_ppool.m
+++ limo_check_ppool.m
@@ -9,16 +9,16 @@ N      = [];
 addons = ver;
 
 if any(strcmpi('Parallel Computing Toolbox',arrayfun(@(x) x.Name, addons, "UniformOutput",false)))
-    
+    % the toolbox exists
     if isempty(N)
         p = gcp('nocreate');
-        if isempty(p) % i.e. the parallel toolbox is not already on
+        if isempty(p) % i.e. the parallel toolbox is not turned on
             
             % check how many cores to use
             % ---------------------------
             N = getenv('NUMBER_OF_PROCESSORS'); % logical number of cores (i.e. count hyperthreading)
             if isempty(N)
-                N = feature('numcores');          % physical number of cores
+                N = feature('numcores');        % physical number of cores
             elseif ischar(N)
                 N = str2double(N);
             end
@@ -35,16 +35,18 @@ if any(strcmpi('Parallel Computing Toolbox',arrayfun(@(x) x.Name, addons, "Unifo
               c.parpool(N-1);
             else
               disp('Parallel toolbox not started - nothing to worry about (except slower computation in some cases)');
+              ps = parallel.Settings;
+              ps.Pool.AutoCreate = false; % if parfor loop, do not try to start ppool
             end
         end
-    else
+    else % this is already running
         try
             parpool(N);
         catch errpool
-            warning('could not start the parallel pool:',errpool.message)
+            warning(errpool.identifier,'could not create a parallel pool: %s',errpool.message)
+            try poolobj = gcp('nocreate'); delete(poolobj); limo_check_ppool; end % close pool and restart
         end
-    end
-    
+    end    
 else
     disp('Parallel toolbox not found - nothing to worry about (except slower computation in some cases)');
 end
