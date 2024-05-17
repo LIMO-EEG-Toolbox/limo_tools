3c41b1598357ba09874ef3ea53d7ba9222aed128
CPernet <wamcyril@gmail.com>
2024-05-17 13:17:59 +0200

    must 1 only

--- limo_central_tendency_and_ci.m
+++ limo_central_tendency_and_ci.m
@@ -321,7 +321,7 @@ elseif nargin == 6 || nargin == 7
         
         if max(parameters) <= sum(LIMO.design.nb_conditions+LIMO.design.nb_interactions) || ...
                 max(parameters) == size(LIMO.design.X,2) || ...% any categorial or the constant
-                any(contains(num2str(parameters),{'1','con'})) % or all con files
+                any([contains(num2str(parameters),{'con'}) strcmp(num2str(parameters),'1')]) % or all con files or 1 
 
             if all(is_limo)               
                 if isnumeric(parameters)
