20d0fa807f3bdddded8cc767aada1438b1445d5e
CPernet <wamcyril@gmail.com>
2025-04-18 13:51:49 +0200

    doc updated

--- limo_neighbourdist.m
+++ limo_neighbourdist.m
@@ -5,15 +5,20 @@ function channeighbstructmat = limo_neighbourdist(EEG, neighbourdist)
 % multiple comparisons. A second optional argument indicate the neighbourhood
 % distance. If it is not provided, the function pops up a GUI to query for it.
 %
-% For explanations:
-% >>help limo_ft_neighbourselection
+% FORMAT: channeighbstructmat = limo_neighbourdist(EEG, neighbourdist)
+% 
+% INPUT EEG is the EEGLAB structure with one or more datasets
+%       neighbourdist is the distance used the distinguish among electrodes
+%       For explanations do > help limo_ft_neighbourselection
+%
+% OUTPUT channeighbstructmat is a structure with the
+%        - expected_chanlocs: the location of electrodes seen in all EEG datasets present
+%        - channeighbstructmat: a binary square matrix specifying neighbours
 %
 % Maris, E., & Oostenveld, R. (2007). 
 % Nonparametric statistical testing of EEG- and MEG-data. 
 % J Neurosci Methods, 164(1), 177-190.
-%
 % http://fieldtrip.fcdonders.nl/tutorial/cluster_permutation_timelock
-%
 % See also LIMO_FT_NEIGHBOURSELECTION
 %
 % Guillaume Rousselet v1 11 July 2010
@@ -21,11 +26,11 @@ function channeighbstructmat = limo_neighbourdist(EEG, neighbourdist)
 % ------------------------------
 %  Copyright (C) LIMO Team 2019
 
-tmpcfg = limo_eeglab2fieldtrip(EEG, 'preprocessing', 'none');
-lay = limo_ft_prepare_layout(tmpcfg, tmpcfg); % fieldtrip function
-tmpcfg.layout        = lay;
 if nargin < 2
     neighbourdist = eval(cell2mat(inputdlg('enter neighbourhood distance','neighbourhood distance'))); % 0.37 for biosemi 128;
 end
-tmpcfg.neighbourdist = neighbourdist;
+tmpcfg                  = limo_eeglab2fieldtrip(EEG, 'preprocessing', 'none');
+lay                     = limo_ft_prepare_layout(tmpcfg, tmpcfg); % fieldtrip function
+tmpcfg.layout           = lay;
+tmpcfg.neighbourdist    = neighbourdist;
 [~,channeighbstructmat] = limo_ft_neighbourselection(tmpcfg, []); % fieldtrip function
