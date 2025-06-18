2cdfcdf22f8e7cbefe6e172328f3b6ec5e8040c3
CPernet <wamcyril@gmail.com>
2025-06-18 14:56:14 +0200

    support sub- as per BIDS

--- limo_batch_contrast.m
+++ limo_batch_contrast.m
@@ -24,8 +24,11 @@ end
 
 fprintf('loading data - and computing contrast(s) in %s \n',pwd)
 
-Yr    = load('Yr');    Yr    = Yr.(cell2mat(fieldnames(Yr)));
-Betas = load('Betas'); Betas = Betas.(cell2mat(fieldnames(Betas)));
+name  = limo_get_subname(fileparts(limo_file));
+Yr    = load(fullfile(fileparts(limo_file),[name '_desc-Yr.mat']));    
+Yr    = Yr.(cell2mat(fieldnames(Yr)));
+Betas = load(fullfile(fileparts(limo_file),[name '_desc-Betas.mat']));    
+Betas = Betas.(cell2mat(fieldnames(Betas)));
 if isfield(LIMO,'contrast')
     previous_con = size(LIMO.contrast,2);
 else
